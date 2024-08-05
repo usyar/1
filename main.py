5714beee962e30e9bd31197d02eb90fabd850881
usyar <kritin_v@protonmail.com>
2024-08-05 20:55:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print(9**0.5*5) # 1st program
-print(9.99>9.98 and 1000!=1000.1) # 2nd program
-print(2*2+2 == 2*(2+2))  # 3rd program
-print (int('123.456'[4])) # 4th program
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+# 3rd program
+print(2*2+2 == 2*(2+2))
+# 4th program
+x=float('123.456')
+print(int((x*10)%10))
